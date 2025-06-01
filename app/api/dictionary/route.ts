7e66eb86631e71548f32064e6e53f17773e45af6
Samuel Konrad <114510161+Samuelk0nrad@users.noreply.github.com>
2025-06-01 09:56:54 +0200

    Updated route.ts

--- app/api/dictionary/route.ts
+++ app/api/dictionary/route.ts
@@ -24,6 +24,36 @@ Do not include any explanations, notes, or additional text before or after the J
 async function getDefinitionFromGemini(
   word: string,
 ): Promise<DictionaryResponse> {
+
+
+    const response = await ai.models.generateContent({
+    model: "gemini-2.0-flash",
+    contents:
+      "List a few popular cookie recipes, and include the amounts of ingredients.",
+    config: {
+      responseMimeType: "application/json",
+      responseSchema: {
+        type: Type.ARRAY,
+        items: {
+          type: Type.OBJECT,
+          properties: {
+            recipeName: {
+              type: Type.STRING,
+            },
+            ingredients: {
+              type: Type.ARRAY,
+              items: {
+                type: Type.STRING,
+              },
+            },
+          },
+          propertyOrdering: ["recipeName", "ingredients"],
+        },
+      },
+    },
+  });
+
+  
   const model = genAI.getGenerativeModel({
     model: "gemini-2.0-flash",
     generationConfig: {
